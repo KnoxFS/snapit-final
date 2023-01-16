90c30f2ab89803cd8cea0f405b7b020c1b594b2a
Gautam Sharma <angrishgauti@hotmail.com>
2023-01-17 00:08:53 +0530

    adding lifetime plan

--- src/pages/api/pro.js
+++ src/pages/api/pro.js
@@ -6,39 +6,50 @@ const YOUR_DOMAIN = process.env.NEXT_PUBLIC_HOST_URL;
 
 export default async function handler(req, res) {
   const { plan, user_id } = req.query;
-  const price_id = '';
-  const mode = '';
-  const url = '';
 
-  if (plan.toLowerCase() === "lifetime") {
-    price_id = "";
-    mode = "payment";
-    url = "onetime";
+  if (plan.toLowerCase() == "lifetime") {
+    const lifetime = "price_1MQwdxE4EcK5n9JayQITodUS";
+
+    if (req.method === "POST") {
+      const session = await stripe.checkout.sessions.create({
+        billing_address_collection: "auto",
+        line_items: [
+          {
+            price: lifetime,
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        allow_promotion_codes: true,
+        success_url: `${YOUR_DOMAIN}/api/onetime?success=true&session_id={CHECKOUT_SESSION_ID}&user_id=${user_id}`,
+        cancel_url: `${YOUR_DOMAIN}/`,
+      });
+
+      res.json({ session_url: session.url, session_id: session.id });
+    }
+
   } else {
-    price_id =
+    const price_id =
     plan.toLowerCase() === "monthly"
       ? process.env.STRIPE_MONTHLY_PRICE_ID
       : process.env.STRIPE_YEARLY_PRICE_ID;
 
-      mode = "subscription";
-      url = "subscription";
-  }
+    if (req.method === "POST") {
+      const session = await stripe.checkout.sessions.create({
+        billing_address_collection: "auto",
+        line_items: [
+          {
+            price: price_id,
+            quantity: 1,
+          },
+        ],
+        mode: "subscription",
+        allow_promotion_codes: true,
+        success_url: `${YOUR_DOMAIN}/api/subscription?success=true&session_id={CHECKOUT_SESSION_ID}&user_id=${user_id}`,
+        cancel_url: `${YOUR_DOMAIN}/`,
+      });
 
-  if (req.method === "POST") {
-    const session = await stripe.checkout.sessions.create({
-      billing_address_collection: "auto",
-      line_items: [
-        {
-          price: price_id,
-          quantity: 1,
-        },
-      ],
-      mode: mode,
-      allow_promotion_codes: true,
-      success_url: `${YOUR_DOMAIN}/api/${url}?success=true&session_id={CHECKOUT_SESSION_ID}&user_id=${user_id}`,
-      cancel_url: `${YOUR_DOMAIN}/`,
-    });
-
-    res.json({ session_url: session.url, session_id: session.id });
+      res.json({ session_url: session.url, session_id: session.id });
+    }
   }
 }
