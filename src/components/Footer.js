bbc1a976c824ca471cf667183e0215571c653674
The KnoxFS Team <56669512+KnoxFS@users.noreply.github.com>
2023-01-24 13:42:19 +0100

    RB

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,10 +19,7 @@ const Footer = () => {
           <a>Community</a>
         </Link>
       </ul>
-      <router>
-      <a href="https://www.producthunt.com/posts/snapit-927d6feb-16f6-42c2-9aa5-98de91904faf?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-snapit&#0045;927d6feb&#0045;16f6&#0045;42c2&#0045;9aa5&#0045;98de91904faf"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=376208&theme=dark" alt="Snapit - Creating&#0032;beautiful&#0032;screenshots&#0032;has&#0032;never&#0032;been&#0032;so&#0032;easy | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
-
-      </router>
+      
       <p>Made with ❤️ in San Francisco.</p>
     </footer>
   );
