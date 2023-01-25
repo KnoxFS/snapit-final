bec2afe468ad4981fc3f64307a848356f3003ec9
The KnoxFS Team <56669512+KnoxFS@users.noreply.github.com>
2023-01-25 18:43:54 +0100

    Add noreferrer

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -56,7 +56,7 @@ const Hero = () => {
               ))}
             </div>
           </ul>
-          <a href="https://www.producthunt.com/posts/snapit-927d6feb-16f6-42c2-9aa5-98de91904faf?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-snapit&#0045;927d6feb&#0045;16f6&#0045;42c2&#0045;9aa5&#0045;98de91904faf" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=376208&theme=dark" alt="Snapit - Creating&#0032;beautiful&#0032;screenshots&#0032;has&#0032;never&#0032;been&#0032;so&#0032;easy | Product Hunt" className=" mt-4 mr-3 width: 250px; height: 54px;" width="250" height="54" /></a>
+          <a href="https://www.producthunt.com/posts/snapit-927d6feb-16f6-42c2-9aa5-98de91904faf?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-snapit&#0045;927d6feb&#0045;16f6&#0045;42c2&#0045;9aa5&#0045;98de91904faf" target="_blank" rel="noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=376208&theme=dark" alt="Snapit - Creating&#0032;beautiful&#0032;screenshots&#0032;has&#0032;never&#0032;been&#0032;so&#0032;easy | Product Hunt" className=" mt-4 mr-3 width: 250px; height: 54px;" width="250" height="54" /></a>
 
         </div>
       </article>
