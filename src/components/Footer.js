1f1582bf437c733cf960825f2f66d17f97aefdd9
Gautam Sharma <angrishgauti@hotmail.com>
2023-04-04 22:58:29 +0530

    changes in footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <p>Snapit - All rights reserved (c) 2023.</p>
 
       <ul className="space-x-4 md:space-x-6 hover:[&>*]:underline my-4 md:my-0">
-        <Link href="/about-us">
+        <Link href="/aboutus">
           <a>About Us</a>
         </Link>
         <Link href="#">
