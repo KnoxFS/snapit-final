ef13ccc8ce46f3258f2028ee138075429837c925
The KnoxFS Team <56669512+KnoxFS@users.noreply.github.com>
2023-04-05 11:31:20 +0200

    test GDPR scrpt

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -35,7 +35,13 @@ function MyApp({ Component, pageProps }) {
             },
           }}
         />
-
+<script
+      id="Cookiebot"
+      src="https://consent.cookiebot.com/uc.js"
+      data-cbid="08d34754-d627-4308-81ed-0b6be79c0888"
+      data-blockingmode="auto"
+      type="text/javascript"
+    ></script>
         {/* Pirsch analytics */}
         <Script
           defer
