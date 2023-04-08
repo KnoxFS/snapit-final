65efc4c5ab94b9fdbaa9152fd3381a89f398c13e
The KnoxFS Team <56669512+KnoxFS@users.noreply.github.com>
2023-04-08 19:02:51 +0200

    Typo

--- src/components/InfoBanner.js
+++ src/components/InfoBanner.js
@@ -5,7 +5,7 @@ const InfoBanner = () => {
     <div className="text-center text-white font-bold p-2 bg-gradient-to-r from-black via-green-400 to-back">
       <Link href="https://kcaps.app/" target="_blank" rel="noreferrer">
         <a className="text-xs md:text-sm">
-          Snapit has acquired KeyCaps. A tool to shortcut keys for your project! Try now for free.
+          Snapit has acquired KeyCaps. A tool to design shortcut keys for your project! Try now for free.
         </a>
       </Link>
     </div>
