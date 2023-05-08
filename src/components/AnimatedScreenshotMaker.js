3fd09aa99a83b2ef7b59500d52f9147b726dbd56
Mehakpal <cdeveloper2016@gmail.com>
2023-05-08 18:46:43 +0530

    updated animated screenshot feature

--- src/components/AnimatedScreenshotMaker.js
+++ src/components/AnimatedScreenshotMaker.js
@@ -131,7 +131,7 @@ const AnimatedScreenshotMaker = ({ proMode }) => {
             >
               {toggle
                 ? type === "mp4" && (
-                    <video controls preload="auto">
+                    <video className="p-20" controls preload="auto">
                       <source
                         src={blob.src}
                         type="video/mp4"
@@ -144,6 +144,7 @@ const AnimatedScreenshotMaker = ({ proMode }) => {
                     <img
                       src={blob.src}
                       className="pointer-events-none h-full w-full  select-none rounded-2xl"
+                      alt="animated image"
                     />
                   )}
             </div>
@@ -180,15 +181,15 @@ const AnimatedScreenshotMaker = ({ proMode }) => {
             <h3 className="text-sm text-gray-400">Format (GIF/MP4)</h3>
             <div className="flex items-center">
               <input
-                class="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-green-400 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] "
+                className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-green-400 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] "
                 type="checkbox"
                 role="switch"
                 id="flexSwitchChecked"
                 onChange={handleToggle}
               />
               <label
-                class="pl-[0.15rem] text-white hover:cursor-pointer"
-                for="flexSwitchChecked"
+                className="pl-[0.15rem] text-white hover:cursor-pointer"
+                htmlFor="flexSwitchChecked"
               >
                 <div className="flex items-center space-x-2">
                   <h3 className="text-sm text-gray-400">
@@ -215,7 +216,7 @@ const AnimatedScreenshotMaker = ({ proMode }) => {
             <div className="flex items-center">
               <div className="mr-2 justify-self-end">⚡</div>
               <input
-                class="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-gray-400 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-green-400 checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-gray-400 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-green-400 checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
                 type="checkbox"
                 role="switch"
                 id="flexSwitchChecked"
@@ -224,8 +225,8 @@ const AnimatedScreenshotMaker = ({ proMode }) => {
                 checked={scrollToggle ? true : false}
               />
               <label
-                class="pl-[0.15rem] text-white hover:cursor-pointer"
-                for="flexSwitchChecked"
+                className="pl-[0.15rem] text-white hover:cursor-pointer"
+                htmlFor="flexSwitchChecked"
               >
                 <div className="flex items-center space-x-2">
                   <h3 className="text-sm text-gray-400">
--- src/components/tools/URLScreenshot.js
+++ src/components/tools/URLScreenshot.js
@@ -1,74 +1,103 @@
-import { useState } from 'react';
-import { toast } from 'react-hot-toast';
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 
-import bufferToBase64 from 'utils/bufferToBase64';
+import bufferToBase64 from "utils/bufferToBase64";
 
 const targets = {
   mobile: { width: 414, height: 896 },
   desktop: { width: 1920, height: 1080 },
 };
 
-const URLScreenshot = ({ proMode, blob, setBlob, target }) => {
-  const [websiteUrl, setWebsiteUrl] = useState('');
-
+const URLScreenshot = ({
+  proMode,
+  blob,
+  setBlob,
+  target,
+  animate = false,
+  animatedWidth,
+  animatedHeight,
+  format,
+  duration,
+  scrollable,
+}) => {
+  const [websiteUrl, setWebsiteUrl] = useState("");
   const getWebsiteScreenshot = async () => {
     if (!proMode) {
-      toast.error('This feature is only available in pro mode');
+      toast.error("This feature is only available in pro mode");
       return;
     }
-
     // valid url with http or https
     if (!websiteUrl.match(/^(http|https):\/\//)) {
-      toast.error('Please enter a valid url (https://example.com).');
+      toast.error("Please enter a valid url (https://example.com).");
       return;
     }
 
     if (websiteUrl.length > 0) {
-      let toastId = toast.loading('Getting website screenshot...');
+      let toastId = toast.loading("Getting website screenshot...");
 
+      const screenshotApi = animate ? "getAnimatedScreenshot" : "getScreenshot";
+      const width = animate ? animatedWidth : targets[target].width;
+      const height = animate ? animatedHeight : targets[target].height;
       const res = await fetch(
-        `/api/getScreenshot?url=${encodeURIComponent(websiteUrl)}&width=${
-          targets[target].width
-        }&height=${targets[target].height}`,
+        `/api/${screenshotApi}?url=${encodeURIComponent(
+          websiteUrl
+        )}&width=${width}&height=${height}&format=${format ? "mp4" : "gif"}${
+          format ? `&duration=${duration}` : ""
+        }${scrollable ? `&scenario=scroll` : ""}`
       );
       const { image, error } = await res.json();
-
       if (error) {
         toast.error(error, { id: toastId });
         return;
       }
 
       const finalImage = bufferToBase64(image.data);
+      setBlob({
+        ...blob,
+        src: `data:${format ? "video/mp4" : "image/png"};base64,${finalImage}`,
+      });
 
-      setBlob({ ...blob, src: `data:image/png;base64,${finalImage}` });
-
-      toast.success('Website screenshot loaded!', { id: toastId });
+      toast.success("Website screenshot loaded!", { id: toastId });
 
       if (window.pirsch) {
-        pirsch('🙌 Screenshot gotten from URL');
+        pirsch("🙌 Screenshot gotten from URL");
       }
 
       return;
     }
 
-    toast.error('No url provided.');
+    toast.error("No url provided.");
     return;
   };
 
   return (
-    <div>
-      <input
-        type='text'
-        placeholder='Enter url, e.g (https://stripe.com)'
-        className='w-full p-2 text-center text-sm bg-darkGreen rounded-md border border-[#2B2C2F] text-white outline-none'
-        value={websiteUrl}
-        onChange={e => setWebsiteUrl(e.target.value)}
-        onKeyUp={e => {
-          if (e.key === 'Enter') {
-            getWebsiteScreenshot();
-          }
-        }}
-      />
+    <div className=" mt-4">
+      <div className="mt-4">
+        <input
+          type="text"
+          placeholder="Enter Url, e.g (https://stripe.com)"
+          className="w-full rounded-md border border-[#2B2C2F] bg-[#212121] p-3 text-center text-sm text-white outline-none"
+          value={websiteUrl}
+          onChange={(e) => setWebsiteUrl(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter" && !animate) {
+              getWebsiteScreenshot();
+            }
+          }}
+        />
+      </div>
+      <div className="mt-4 flex flex-col items-center justify-center">
+        {animate && (
+          <button
+            className="flex items-center justify-center rounded-md bg-green-400 px-4 py-2 text-base text-white"
+            onClick={(e) => {
+              getWebsiteScreenshot();
+            }}
+          >
+            <span className="w=6 h=5 mr-2">Get Screenshot</span>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
