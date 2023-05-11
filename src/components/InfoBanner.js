a25cac567a089b2dcc1b066216acbd8022d7e607
The KnoxFS Team <56669512+KnoxFS@users.noreply.github.com>
2023-05-11 12:54:11 +0200

    Update infobanner

--- src/components/InfoBanner.js
+++ src/components/InfoBanner.js
@@ -5,8 +5,7 @@ const InfoBanner = () => {
     <div className='text-center text-white font-bold p-2 bg-gradient-to-r from-black via-primary to-back'>
       <Link href='/templates'>
         <a className='text-xs md:text-sm'>
-          Introducing Code Snippets - A new way to share screenshots of your
-          code.
+          Introducing Animated Screenshots - Record a website and save as Gif or MP4.
         </a>
       </Link>
     </div>
