d54ed5dc3f7f7ed93cdde2dc385f987c0ee5643f
Gautam Sharma <angrishgauti@hotmail.com>
2024-02-27 23:00:37 +0530

    Remove Google Subscription Code

--- src/pages/api/verifySubscription.js
+++ src/pages/api/verifySubscription.js
@@ -5,13 +5,6 @@ const stripe = new Stripe(process.env.STRIPE_SECREY_KEY);
 export default async function handler(req, res) {
   const { subscription_id } = req.query;
 
-  if(subscription_id.includes('GPA.')) {
-    res
-      .status(200)
-      .json({ active: true, end: subscription_id});
-    return;
-  }
-
   const subscription = await stripe.subscriptions.retrieve(subscription_id);
 
   if (subscription.status === "active") {
