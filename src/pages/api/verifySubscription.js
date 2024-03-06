cac3cc816920f65028629d888188f983b79c62e7
Gautam Sharma <angrishgauti@hotmail.com>
2024-03-06 22:10:27 +0530

    Google Subscription

--- src/pages/api/verifySubscription.js
+++ src/pages/api/verifySubscription.js
@@ -12,20 +12,23 @@ export default async function handler(req, res) {
       return;
   } else {
 
-    const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-      myHeaders.append("Cookie", "__Host-GAPS=1:uZHWhTUFDGpj-atVWCXZ3g8z_88fnA:hSxh_O5N_bM_zQjz");
+    var settings = {
+      "url": "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=607904390113-7ldijsov15clj744qtvao177gemovcvd.apps.googleusercontent.com&redirect_uri=http://screenshots4all.com/callback&access_type=offline&scope=https://www.googleapis.com/auth/androidpublisher",
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Cookie": "__Host-GAPS=1:uZHWhTUFDGpj-atVWCXZ3g8z_88fnA:hSxh_O5N_bM_zQjz"
+      },
+    };
 
-      const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow"
-      };
-
-      fetch("https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=607904390113-7ldijsov15clj744qtvao177gemovcvd.apps.googleusercontent.com&redirect_uri=http://screenshots4all.com/callback&access_type=offline&scope=https://www.googleapis.com/auth/androidpublisher", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      res
+      .status(200)
+      .json({ active: 'fetch data', message: response });
+      return;
+    });
 
     // const subscription = await stripe.subscriptions.retrieve(subscription_id);
 
@@ -35,10 +38,5 @@ export default async function handler(req, res) {
     //     .json({ active: true, end: subscription.current_period_end });
     //   return;
     // }
-
-    res
-      .status(200)
-      .json({ active: 'fetch data', message: result, error: error });
-      exit();
   }
 }
