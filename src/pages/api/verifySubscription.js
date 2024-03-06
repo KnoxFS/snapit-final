621aa12da9e5bb869f78c7a7de17888898715554
Gautam Sharma <angrishgauti@hotmail.com>
2024-03-06 22:42:02 +0530

    Google Subscription

--- src/pages/api/verifySubscription.js
+++ src/pages/api/verifySubscription.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
     const {callbackdata} = await fetch(
       'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=607904390113-7ldijsov15clj744qtvao177gemovcvd.apps.googleusercontent.com&redirect_uri=http://screenshots4all.com/callback&access_type=offline&scope=https://www.googleapis.com/auth/androidpublisher'
-      ).then((rescall) => rescall.json());
+      ).then((rescall) => console.log(rescall));
 
       if (callbackdata) {
         res
