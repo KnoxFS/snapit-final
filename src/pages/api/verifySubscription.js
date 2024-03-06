5ab597b862e8cec4dd78829971d5e33524e3b130
Gautam Sharma <angrishgauti@hotmail.com>
2024-03-06 21:54:37 +0530

    Google Subscription

--- src/pages/api/verifySubscription.js
+++ src/pages/api/verifySubscription.js
@@ -12,18 +12,33 @@ export default async function handler(req, res) {
       return;
   } else {
 
-    const subscription = await stripe.subscriptions.retrieve(subscription_id);
+    const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+      myHeaders.append("Cookie", "__Host-GAPS=1:uZHWhTUFDGpj-atVWCXZ3g8z_88fnA:hSxh_O5N_bM_zQjz");
 
-    if (subscription.status === "active") {
-      res
-        .status(200)
-        .json({ active: true, end: subscription.current_period_end });
-      return;
-    }
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+      };
 
-    res
-      .status(200)
-      .json({ active: false, message: "Subscription is not active." });
-      return;
+      fetch("https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=607904390113-7ldijsov15clj744qtvao177gemovcvd.apps.googleusercontent.com&redirect_uri=http://screenshots4all.com/callback&access_type=offline&scope=https://www.googleapis.com/auth/androidpublisher", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+
+    // const subscription = await stripe.subscriptions.retrieve(subscription_id);
+
+    // if (subscription.status === "active") {
+    //   res
+    //     .status(200)
+    //     .json({ active: true, end: subscription.current_period_end });
+    //   return;
+    // }
+
+    // res
+    //   .status(200)
+    //   .json({ active: false, message: "Subscription is not active." });
+    //   return;
   }
 }
