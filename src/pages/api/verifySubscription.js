dab16692f89f2d6de3ddba13e93c9b909b25b4c9
Gautam Sharma <angrishgauti@hotmail.com>
2024-03-06 19:46:42 +0530

    Google Subscription

--- src/pages/api/verifySubscription.js
+++ src/pages/api/verifySubscription.js
@@ -5,18 +5,6 @@ const stripe = new Stripe(process.env.STRIPE_SECREY_KEY);
 export default async function handler(req, res) {
   const { subscription_id } = req.query;
 
-  var splitVal = subscription_id.split('.');
-  if (splitVal.length == 0) {
-    splitVal = subscription_id.split('_');
-  }
-
-  if (splitVal[0] == 'GPA') {
-    res
-        .status(200)
-        .json({ active: 'checking'});
-      return;
-  } else {
-
     const subscription = await stripe.subscriptions.retrieve(subscription_id);
 
     if (subscription.status === "active") {
@@ -30,5 +18,4 @@ export default async function handler(req, res) {
       .status(200)
       .json({ active: false, message: "Subscription is not active." });
       return;
-  }
 }
